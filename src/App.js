eae3ff7bfb0d7a1f2f8767e9df12ff2224b3a4fe
Ranveer88098 <amansinghrajput898661@gmail.com>
2023-01-31 14:13:51 +0530

    counting

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import { Segment,Button,Header  } from "semantic-ui-react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  const Inc=()=>{
+    setCount(count+1);
+  }
+  const Dec=()=>{
+    setCount(count-1);
+  }
+  return (
+    <>
+
+
+
+    <Segment  style={{ margin: "10% 38%", textAlign: "center", fontSize: "25px",  padding: "3% 1%", borderRadius: "8px" }}>
+    <h1>Count</h1>
+    <h1>{count}</h1>
+
+    <div>
+    
+    <Button  positive onClick={Inc} primary>Increment </Button>
+    <Button negative disabled={count===0} onClick={Dec} secondary>Decrement</Button>
+  </div>
+   
+ 
+   
+      </Segment>
+
+      <Header as='h3' textAlign='center'>
+      Done by:-Ranveer kumar singh (12006211)
+    </Header>
+      
+      
+    </>
+  );
+}
+export default App;
